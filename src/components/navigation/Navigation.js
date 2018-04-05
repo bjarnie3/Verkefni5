8110e7d85119259009195f43acf03322a976161f
bjarnie3 <bjarnie3@msn.com>
2018-04-05 22:59:51 +0000

    final
    final útgáfa

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './Navigation.css';
 const url = process.env.REACT_APP_SERVICE_URL;
 
@@ -7,7 +8,27 @@ const url = process.env.REACT_APP_SERVICE_URL;
 /* hér ætti að sækja gögn frá vefþjónustu fyrir valmynd */
 
 export default class Navigation extends Component {
-  state = { data: null, loading: true, error: false }
+  static propTypes = {
+    title: PropTypes.string,
+    course: PropTypes.string,
+    name: PropTypes.string,
+    students: PropTypes.string,
+    date: PropTypes.instanceOf(Date),
+    visibleSvid: PropTypes.bool,
+    toggleBold2: PropTypes.func,
+  }
+
+  state = { data: null, loading: true, error: false, visisbleSvid: false }
+
+  static defaultProps = {
+  visisbleSvid: false,
+  toggleBold2: () => {
+    },
+  }
+
+  changeColor(){
+      this.setState({invisibleSvid: !this.props.visibleSvid})
+  }
 
   async componentDidMount() {
     try {
@@ -25,8 +46,23 @@ export default class Navigation extends Component {
     return data;
   }
 
+  toggleBold = (id) =>  {
+    const element = document.getElementById('felagsvisindasvid');
+    //element.style.fontWeight = "bold"
+  }
+
+// Finn ekki út hvernig á að toggle bold
+  toggleBold2 = (id) => {
+    return (e) => {
+      const visibleSvid = this.state.visibleSvid === id ? null : id;
+      this.setState({ visibleSvid });
+    }
+  }
+
   render() {
     const { data, loading, error } = this.state;
+    const display = this.props.isVisble ? 'font-weight: normal' : 'font-weight: bold';
+
     if (loading) {
       return (<div>Sæki gögn...</div>);
     }
@@ -43,7 +79,10 @@ export default class Navigation extends Component {
             <ul>
               {data.schools.map((item, i) => (
                 <li key={i} id={i} className="nav-li">
-                  <NavLink to={`${item.slug}`}>{item.name}</NavLink>
+                  <NavLink to={`${item.slug}`}
+                  id={item.slug}
+                  onClick={this.changeColor.bind(this)}>
+                  {item.name}</NavLink>
                 </li>
               ))}
             </ul>
